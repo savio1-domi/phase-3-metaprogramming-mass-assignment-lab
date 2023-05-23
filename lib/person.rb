e8926d1e5bacb4f067716346b536310a15f8cc46
git-savio1.domi <techsavvydominic3@gmail.com>
2023-05-23 10:15:46 +0300

    Completed assignments

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,11 @@
 class Person
   # your code here
+
+  # attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+  def initialize(hash)
+    hash.each do |item, value|
+      self.class.attr_accessor(item)
+      self.send("#{item}=", value)
+    end
+  end
 end
